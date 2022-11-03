bb9dcdf0ea90ce1fa21291f646052f3c2b5ad6b9
atapias86 <atapias86@misena.edu.co>
2022-11-03 12:29:18 -0500

    Ejercicio 6

--- api.php
+++ api.php
@@ -3,12 +3,23 @@
     header('Content-Type: application/json; charset=utf-8');
     $_DATA = json_decode(file_get_contents("php://input"),true);
 
-    class pago{
+    class promedio{
+        public $lista;
+
         public function __construct($lista){
 
+            $this->lista = $lista;
         }
+        public function masa():string{
+            $lista = $this->lista;
 
-        public function ventas():string{
-        }
+            $contador= count($lista);
+            $suma= array_sum($lista);
+
+            return $suma/$contador;
     }
+}
+
+$lista = new promedio($_DATA);
+echo($lista->masa());
 ?>
--- main.js
+++ main.js
@@ -1,77 +1,46 @@
-let ventaMen = 0, ventaMed = 0, ventaSup = 0, canMen = 0, canMed = 0, canSup = 0, ventaTotal = 0;
-
-function sumaVentas(precio) {
-    precio <= 500 ?
-        (ventaMen += precio, canMen++) :
-        precio <= 1000 && precio > 500 ?
-            (ventaMed += precio, canMed++) :
-            (ventaSup += precio, canSup++);
-    ventaTotal = ventaMen + ventaMed + ventaSup;
+function masa(presion,volumen){
+    return presion*volumen;
 }
 
 addEventListener("DOMContentLoaded", (e) => {
     document.querySelector("#cantidad").removeAttribute("disabled");
-    document.querySelector(".submit").removeAttribute("disabled");
     document.querySelector("#cantidad").addEventListener("keyup", (e) => {
-        let precio = document.querySelector("#precio");
+        let presion = document.querySelector("#presion");
+        let volumen = document.querySelector("#volumen");
         if (e.target.value > 0) {
-            precio.removeAttribute("disabled");
+            presion.removeAttribute("disabled");
+            volumen.removeAttribute("disabled");
         } else {
-            precio.disabled = true;
+            presion.disabled = true;
+            volumen.disabled = true;
         }
-        let con = 1;
-        let myform = document.querySelector("#form");
-        myform.addEventListener("submit", async (e) => {
-            e.preventDefault();
-            let cantidad = document.querySelector("#cantidad").value;
-            let precio = parseInt(document.querySelector("#precio").value);
-            document.querySelector("#cantidad").disabled = true;
-            if (con < cantidad) {
-                document.querySelector("#precio").value = "";
-                con += 1;
-                sumaVentas(precio);
-
-            } else {
-                document.querySelector("#precio").disabled = true;
-                document.querySelector(".submit").disabled = true;
-                document.querySelector(".Limpiar").removeAttribute("disabled");
-                document.querySelector("#res").innerHTML = "";
-                sumaVentas(precio);
-                let plantillas = `
-                <tr>
-                    <td>${"Superiores a 1000"}</td>
-                    <td>${canSup}</td>
-                    <td>${ventaSup}</td>
-                </tr>
-                <tr>
-                    <td>${"superiores a 500 y menores a 1000"}</td>
-                    <td>${canMed}</td>
-                    <td>${ventaMed}</td>
-                </tr>
-                <tr>
-                    <td>${"menores a 500"}</td>
-                    <td>${canMen}</td>
-                    <td>${ventaMen}</td>
-                </tr>
-                <tr>
-                    <td colspan="2">${"venta total"}</td>
-                    <td>${ventaTotal}</td>
-                </tr>`;
-                document.querySelector("#dataTable_carreras").insertAdjacentHTML("beforeend", plantillas);
-                con = 1;
-            }
-        })
     })
-    let limpiar = document.querySelector(".Limpiar");
-    limpiar.addEventListener("click", (e) => {
+    let con = 1,lista=[];
+    let myform = document.querySelector("#form");
+    myform.addEventListener("submit", async (e) => {
         e.preventDefault();
-        let myfor = document.querySelector("#form");
-        myfor.reset();
-        document.querySelector("#cantidad").removeAttribute("disabled");
-        document.querySelector(".Limpiar").disabled = true;
-        document.querySelector(".submit").removeAttribute("disabled");
-        document.querySelector("#dataTable_carreras").innerHTML = "";
-        ventaMen = 0, ventaMed = 0, ventaSup = 0, canMen = 0, canMed = 0, canSup = 0, ventaTotal = 0;
-        con = 1;
+        let cantidad = document.querySelector("#cantidad").value;
+        let presion = parseInt( document.querySelector("#presion").value);
+        let volumen = parseInt( document.querySelector("#volumen").value);
+        document.querySelector("#cantidad").disabled = true;
+        if (con < cantidad) {
+            document.querySelector("#presion").value = "";
+            document.querySelector("#volumen").value = "";
+            con += 1;
+            lista.push(masa(presion,volumen));
+        } else {
+            let form = e.target;
+            lista.push(masa(presion,volumen));
+            document.querySelector("#presion").disabled=true;
+            document.querySelector("#volumen").disabled=true;
+            let config = {
+                method: form.method,
+                body: JSON.stringify(lista)
+            };
+
+            let peticion = await fetch(form.action, config);
+            let respuesta = await peticion.text();
+            document.querySelector("#res").insertAdjacentHTML("beforeend",respuesta)
+        }
     })
 })
