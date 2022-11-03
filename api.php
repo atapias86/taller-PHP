159c0489128e8115f8759fa10432055d7de98d14
atapias86 <atapias86@misena.edu.co>
2022-11-03 12:16:27 -0500

    Ejercicio 5

--- api.php
+++ api.php
@@ -4,8 +4,11 @@
     $_DATA = json_decode(file_get_contents("php://input"),true);
 
     class pago{
-        public function __construct(){
+        public function __construct($lista){
+
+        }
+
+        public function ventas():string{
         }
     }
-    $lista = new pago();
 ?>
--- main.js
+++ main.js
@@ -1,32 +1,77 @@
-addEventListener("DOMContentLoaded", (e) => {
-    let form = document.querySelector("#form");
-    form.addEventListener("submit", async (e) => {
-        e.preventDefault();
-        let meses = document.querySelector("#meses").value;
-        let dato = new Date(meses);
-        let year, mes, dia;
-        var pago = 10;
-        if (!!dato.valueOf()) {
-            year = dato.getFullYear();
-            mes = dato.getMonth() + 1;
-            dia = dato.getDate() + 1;
-        }
-        let cont = 1;
-        let diasSemana = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
-        let nombreMes = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-        for (let i = 0; i < 20; i++) {
-            var indice = new Date(year, mes - 1, 16).getDay();
-            let plantillas = `
-            <tr>
-                <td>${cont}</td>
-                <td>${dia + "/"} ${nombreMes[new Date(year, mes - 1, dia).getMonth()] + "/"} ${(new Date(year, mes - 1, dia).getMonth() == 0) ? ++year : year}</td>
-                <td>${pago}</td>
-            </tr>`;
-            document.querySelector("#dataTable_carreras").insertAdjacentHTML("beforeend", plantillas);
-            cont ++;
-            pago = pago * 2;
-            mes++;
-        }
+let ventaMen = 0, ventaMed = 0, ventaSup = 0, canMen = 0, canMed = 0, canSup = 0, ventaTotal = 0;
 
+function sumaVentas(precio) {
+    precio <= 500 ?
+        (ventaMen += precio, canMen++) :
+        precio <= 1000 && precio > 500 ?
+            (ventaMed += precio, canMed++) :
+            (ventaSup += precio, canSup++);
+    ventaTotal = ventaMen + ventaMed + ventaSup;
+}
+
+addEventListener("DOMContentLoaded", (e) => {
+    document.querySelector("#cantidad").removeAttribute("disabled");
+    document.querySelector(".submit").removeAttribute("disabled");
+    document.querySelector("#cantidad").addEventListener("keyup", (e) => {
+        let precio = document.querySelector("#precio");
+        if (e.target.value > 0) {
+            precio.removeAttribute("disabled");
+        } else {
+            precio.disabled = true;
+        }
+        let con = 1;
+        let myform = document.querySelector("#form");
+        myform.addEventListener("submit", async (e) => {
+            e.preventDefault();
+            let cantidad = document.querySelector("#cantidad").value;
+            let precio = parseInt(document.querySelector("#precio").value);
+            document.querySelector("#cantidad").disabled = true;
+            if (con < cantidad) {
+                document.querySelector("#precio").value = "";
+                con += 1;
+                sumaVentas(precio);
+
+            } else {
+                document.querySelector("#precio").disabled = true;
+                document.querySelector(".submit").disabled = true;
+                document.querySelector(".Limpiar").removeAttribute("disabled");
+                document.querySelector("#res").innerHTML = "";
+                sumaVentas(precio);
+                let plantillas = `
+                <tr>
+                    <td>${"Superiores a 1000"}</td>
+                    <td>${canSup}</td>
+                    <td>${ventaSup}</td>
+                </tr>
+                <tr>
+                    <td>${"superiores a 500 y menores a 1000"}</td>
+                    <td>${canMed}</td>
+                    <td>${ventaMed}</td>
+                </tr>
+                <tr>
+                    <td>${"menores a 500"}</td>
+                    <td>${canMen}</td>
+                    <td>${ventaMen}</td>
+                </tr>
+                <tr>
+                    <td colspan="2">${"venta total"}</td>
+                    <td>${ventaTotal}</td>
+                </tr>`;
+                document.querySelector("#dataTable_carreras").insertAdjacentHTML("beforeend", plantillas);
+                con = 1;
+            }
+        })
+    })
+    let limpiar = document.querySelector(".Limpiar");
+    limpiar.addEventListener("click", (e) => {
+        e.preventDefault();
+        let myfor = document.querySelector("#form");
+        myfor.reset();
+        document.querySelector("#cantidad").removeAttribute("disabled");
+        document.querySelector(".Limpiar").disabled = true;
+        document.querySelector(".submit").removeAttribute("disabled");
+        document.querySelector("#dataTable_carreras").innerHTML = "";
+        ventaMen = 0, ventaMed = 0, ventaSup = 0, canMen = 0, canMed = 0, canSup = 0, ventaTotal = 0;
+        con = 1;
     })
 })
